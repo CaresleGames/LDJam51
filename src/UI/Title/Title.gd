0894fd8c5a08222a04b22b569eaae71025cc5fb7
Caresle <carabesle@gmail.com>
2022-10-02 17:47:51 -0600

    Update to godot 3.5.1

--- src/UI/Title/Title.gd
+++ src/UI/Title/Title.gd
@@ -2,6 +2,13 @@ extends Control
 
 export var scene : PackedScene
 
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("ui_jump"):
+		get_tree().change_scene_to(scene)
+	if Input.is_action_just_pressed("ui_exit"):
+		get_tree().quit()
+
 func _on_Exit_pressed() -> void:
 	get_tree().quit()
 
